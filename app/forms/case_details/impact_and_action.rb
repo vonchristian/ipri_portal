6e0d56ba89d399b3ea16f65e8dfa5555ce6c9221
Von Christian Halip <vc.halip@gmail.com>
2022-09-03 12:08:50 +0800

    Updates

--- app/forms/case_details/impact_and_action.rb
+++ app/forms/case_details/impact_and_action.rb
@@ -4,7 +4,8 @@ module CaseDetails
   class ImpactAndAction
     include ActiveModel::Model
 
-    attr_accessor :impact_to_victim_details, :impact_to_community_details, :actions_taken_status, :actions_taken_details, :case_detail_id
+    attr_accessor :impact_to_victim_details, :impact_to_community_details, :actions_taken_status,
+      :actions_taken_details, :case_detail_id
 
     validates :impact_to_victim_details, :impact_to_community_details, :actions_taken_details, presence: true
 
@@ -17,12 +18,16 @@ module CaseDetails
     private
 
     def set_impact_and_actions_taken
-      CaseDetails::CaseDetail.find(case_detail_id).update_columns(
-        impact_to_victim_details: impact_to_victim_details,
+      find_case_detail.update_columns(
+        impact_to_victim_details:    impact_to_victim_details,
         impact_to_community_details: impact_to_community_details,
-        actions_taken_details: actions_taken_details,
-        actions_taken_status: actions_taken_status
+        actions_taken_details:       actions_taken_details,
+        actions_taken_status:        actions_taken_status
       )
+    end
+
+    def find_case_detail
+      CaseDetails::CaseDetail.find(case_detail_id)
     end
   end
 end
