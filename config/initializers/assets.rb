06ce3bda1715a1714629da6274d1ae495a37a18e
Von Christian Halip <vc.halip@gmail.com>
2023-06-07 13:59:21 +0800

    test fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -10,3 +12,28 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+require "sprockets/sass_compressor"
+class Sprockets::SassCompressor
+  TAILWIND_SEARCH = "--tw-"
+  def call(*args)
+    input = if defined?(data)
+      data # sprockets 2.x
+    else
+      args[0][:data] # sprockets 3.x
+    end
+
+    return input if skip_compiling?(input) # added this line
+
+    SassC::Engine.new(
+      input,
+      {
+        style: :compressed,
+      },
+    ).render
+  end
+
+  def skip_compiling?(body)
+    body.include?(TAILWIND_SEARCH)
+  end
+end
