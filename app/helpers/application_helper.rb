08cb91de69ce547d5f553c8a3585eb4eef8c09e6
Von Christian Halip <vc.halip@gmail.com>
2025-02-21 13:52:26 +0800

    Fix paginations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,31 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  def pagy_nav_custom(pagy)
+    link_class = "px-4 py-2 border rounded-md hover:bg-gray-100"
+    active_class = "bg-gray-200 text-white font-bold"
+
+    html = '<nav class="flex justify-center space-x-2 mt-4">'.html_safe
+
+    # Previous link
+    html << (pagy.prev ? link_to("Prev", url_for(page: pagy.prev), class: link_class) : '<span class="opacity-50 px-4 py-2 border rounded-md">Prev</span>'.html_safe)
+
+    # Page links
+    pagy.series.each do |item|
+      html << if item.is_a?(Integer)
+                link_to(item, url_for(page: item), class: [link_class, (active_class if item == pagy.page)].compact.join(" "))
+              else
+                "<span class='px-4 py-2'>#{item}</span>".html_safe
+              end
+    end
+
+    # Next link
+    html << (pagy.next ? link_to("Next", url_for(page: pagy.next), class: link_class) : '<span class="opacity-50 px-4 py-2 border rounded-md">Next</span>'.html_safe)
+
+    html << '</nav>'.html_safe
+    html
+  end
+
+
 end
