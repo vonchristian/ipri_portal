68abc5bf0176c124cdab91516617adf3b2f6de4f
Von Christian Halip <vc.halip@gmail.com>
2025-03-14 19:41:07 +0800

    Fix

--- Gemfile
+++ Gemfile
@@ -93,9 +93,6 @@ gem "devise"
 gem "acts-as-taggable-on"
 gem 'counter_culture', '~> 3.2'
 gem "cssbundling-rails"
-
-gem "tailwindcss-rails", "~> 3.0"
 gem "ffi"
 gem 'caxlsx'
 gem 'caxlsx_rails'
-gem "tailwindcss-ruby", "~> 3.4"
--- config/initializers/tailwindcss.rb
+++
@@ -1,4 +0,0 @@
-if Rails.env.production?
-  tailwindcss_binary_path = File.join(Rails.root, 'vendor', 'bundle', 'ruby', '3.4.0', 'gems', 'tailwindcss-ruby-3.4.17', 'exe', 'tailwindcss')
-  ENV['TAILWINDCSS_BINARY'] = tailwindcss_binary_path if File.exist?(tailwindcss_binary_path)
-end
